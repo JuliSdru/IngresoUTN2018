3d5fa31cc215638b87b8371f8af88c9ac917efb5
JuliSdru <36230712+JuliSdru@users.noreply.github.com>
2018-02-21 10:13:29 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (5).js
+++ 5-InstruccionWhile/jsIteraciones (5).js
@@ -2,6 +2,16 @@ function Mostrar()
 {
 
 var sexo = prompt("ingrese f ó m .");
+sexo = sexo.toLowerCase();
+// se puede poner para que la condicion sea invalida 
+// se puede poner pra que la condicion sea valida y se niegue
+// to Lower Case para pasar a minuscula
+
+while ( !(sexo == 'f' || sexo == 'm'))
+{
+    sexo = prompt ("Error. Reingrese sexo ");
+    sexo =toLowerCase();
+}
 
 
 
