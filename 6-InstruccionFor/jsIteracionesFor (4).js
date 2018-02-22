d22838be2ce0a2b6860e8d442c17b92b6de51410
JuliSdru <36230712+JuliSdru@users.noreply.github.com>
2018-02-22 12:22:33 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (4).js
+++ 6-InstruccionFor/jsIteracionesFor (4).js
@@ -1,6 +1,14 @@
 function Mostrar()
 {
-
+    for(var i = 0;     ; i++)
+    {
+        if(i == 1000000)
+        {
+            break;
+        }
+    }
+    
+    alert ("listo");
 
 
 
--- 6-InstruccionFor/jsIteracionesFor (5).js
+++ 6-InstruccionFor/jsIteracionesFor (5).js
@@ -1,5 +1,21 @@
 function Mostrar()
 {
+  for (   ;     ;  )
+{
+    i = parseInt(prompt("ingrese un numero  "));
+    if (i == 9)
+    {
+        break;
+
+    }
+   
+    alert(numero);
+
+}
+
+    
+
+alert("salir por ingreso de numero 9");
 
 
 
